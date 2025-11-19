e12d40fea26144207f5549750ed1763c3189d15f
thecreativeAVI <avinashkale0124@gmail.com>
2025-11-19 13:18:59 +0530

    Done Nigga!

---
+++ Arrays/2154.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        i=0
+        nums.sort()
+        while i <len(nums):
+            if nums[i]==original:
+                original=original*2
+                i=0
+                continue
+            i+=1
+        return original
+        
