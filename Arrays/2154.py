bae8e11ee94b421a97b5d467d1ff0b7cc3adae3c
thecreativeAVI <avinashkale0124@gmail.com>
2025-11-19 13:29:33 +0530

    DOne with another approach!

--- Arrays/2154.py
+++ Arrays/2154.py
@@ -10,3 +10,10 @@ class Solution(object):
             i+=1
         return original
         
+
+
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        while original in nums:
+                original *= 2
+        return(original)
